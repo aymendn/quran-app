7ea64fdab8f520d3a31d81c2a0dfac77387aef67
Aymen Dennoub <64363186+aymendn@users.noreply.github.com>
2025-04-01 10:14:50 +0100

    fix: update macOS assets and identifiers
    This commit ensures that the macOS application assets and identifiers are updated for consistency and proper functionality.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-import shared_preferences_macos
+import connectivity_plus
+import flutter_archive
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
